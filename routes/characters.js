1d9b325652486e208440e72dac8f4cd5d2c5750e
Ekaterina Jégo <cat-velichko@yandex.ru>
2021-08-11 15:22:04 +0200

    modifications

--- routes/characters.js
+++ routes/characters.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+require("dotenv").config();
 
 // FICHE TECHNIQUE
 // Sur la page principale « personnages », vous devrez faire apparaître la liste des personnages MARVEL, par ordre alphabétique (100 par page), sous forme de fiche (photo, nom, description). En cliquant sur chaque fiche, il devra être possible d’accéder à une page regroupant les comics liés au personnage.
@@ -9,13 +10,10 @@ const axios = require("axios");
 
 router.get("/characters", async (req, res) => {
   try {
-    const response = await axios
-      .get(`${process.env.API}/characters?apiKey=${process.env.API_SECRET_KEY}`)
-      .then((response) => {
-        res.json("Hi");
-        res.json(response.data);
-        console.log(response.data);
-      });
+    const response = await axios.get(
+      `${process.env.API}/characters?apiKey=${process.env.API_SECRET_KEY}`
+    );
+
     res.status(200).json(response.data);
     console.log(response.data);
   } catch (error) {
--- routes/comics.js
+++ routes/comics.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+require("dotenv").config();
 
 // FICHE TECHNIQUE
 // Sur la page « comics », vous devrez faire apparaître la liste des comics MARVEL, par ordre alphabétique, sous forme de fiche (photo, titre, description).
@@ -9,13 +10,9 @@ const axios = require("axios");
 
 router.get("/comics", async (req, res) => {
   try {
-    const response = await axios
-      .get(`${process.env.API}/comics?apiKey=${process.env.API_SECRET_KEY}`)
-      .then((response) => {
-        res.json("Hi");
-        res.json(response.data);
-        console.log(response.data);
-      });
+    const response = await axios.get(
+      `${process.env.API}/comics?apiKey=${process.env.API_SECRET_KEY}`
+    );
   } catch (error) {
     res.status(400).json(error.message);
   }
@@ -29,7 +26,6 @@ router.get("/comics/:characterId", async (req, res) => {
     const response = axios.await(
       `${process.env.API}/comics/${characterId}?apiKey=${process.env.API_SECRET_KEY}`
     );
-    const object = {};
     res.json(response.data);
   } catch (error) {
     res.status(400).json(error.message);
